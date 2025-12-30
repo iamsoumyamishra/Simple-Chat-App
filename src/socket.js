f41eefb3def8b37467b34d4926dbb3cb89465554
Soumya <soumyamishra2099@gmail.com>
2025-12-30 15:58:12 +0530

    TEST

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket_url = "http://localhost:3000";
+const socket_url = "https://73763183e4ff.ngrok-free.app";
 
 export const socket = io(socket_url, {
     autoConnect: false

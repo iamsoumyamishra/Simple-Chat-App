5b5eb29adac610d04dc50f110289cc7305e8c340
Soumya <soumyamishra2099@gmail.com>
2025-12-30 16:24:46 +0530

    FIX

--- src/components/Logs.jsx
+++ src/components/Logs.jsx
@@ -1,16 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 
 
 const Logs = ({logs = []}) => {
 
+        const messagesEndRef = useRef(null);
+    
+        const scrollToBottom = () => {
+            messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+        }
+    
+        useEffect(() => {
+            scrollToBottom();
+        }, [msgArr])
 
     return (
-        <div className='logs rounded py-1 px-2 bg-black text-white h-120 w-120'>
+        <div className='logs rounded py-1 px-2 bg-black text-white h-120 w-120 overflow-y-auto'>
             {logs.map((log, index) => {
                 return <div key={index}>
                     {log}
                 </div>
             })}
+            <div ref={messagesEndRef}></div>
         </div>
     )
 }
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -13,14 +13,15 @@ const Message = ({ user = { name: "user" }, msgArr = [] }) => {
     }, [msgArr])
 
     return (
-        <div className='bg-white overflow-auto logs flex flex-col rounded border-2 py-1 px-2 h-120 w-120'>
+        <div className='bg-white overflow-y-auto logs flex flex-col rounded border-2 py-1 px-2 h-120 w-120'>
             {
                 msgArr.map((msg, index) => {
                     return (
-                        <div key={index} ref={messagesEndRef} className={`flex py-1 px-2 m-2 ${msg?.role === user ? "self-end bg-green-400 text-white rounded" : "self-start"}`}>{msg?.msg}</div>
+                        <div key={index} className={`flex py-1 px-2 m-2 ${msg?.role === user ? "self-end bg-green-400 text-white rounded" : "self-start"}`}>{msg?.msg}</div>
                     )
                 })
             }
+            <div ref={messagesEndRef}></div>
         </div>
     )
 }

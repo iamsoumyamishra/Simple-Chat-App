e3232691b874093c35625cb29a9e85394dd4baf6
Soumya <soumyamishra2099@gmail.com>
2025-12-30 16:10:47 +0530

    FIX

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 
-const Message = ({user = {name: "user"}, msgArr = []}) => {
+const Message = ({ user = { name: "user" }, msgArr = [] }) => {
+
+    const messagesEndRef = useRef(null);
+
+    const scrollToBottom = () => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    }
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [msgArr])
 
     return (
-        <div className='bg-white logs flex flex-col rounded border-2 py-1 px-2 h-120 w-120'>
+        <div className='bg-white overflow-auto logs flex flex-col rounded border-2 py-1 px-2 h-120 w-120'>
             {
                 msgArr.map((msg, index) => {
-                    return(
-                        <div key={index} className={`flex py-1 px-2 m-2 ${msg?.role === user? "self-end bg-green-400 text-white rounded" :"self-start"}`}>{msg?.msg}</div>
+                    return (
+                        <div key={index} ref={messagesEndRef} className={`flex py-1 px-2 m-2 ${msg?.role === user ? "self-end bg-green-400 text-white rounded" : "self-start"}`}>{msg?.msg}</div>
                     )
                 })
             }
